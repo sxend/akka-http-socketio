d95a16d096d598970d742dfe0bd1ade8bb62eef7
sxend <arimitsu.k+github@gmail.com>
2016-11-05 18:25:58 +0900

    add sources

---
+++ src/test/scala/arimitsu/sf/akka/http/socketio/HelloWorldSpec.scala
@@ -0,0 +1,27 @@
+package arimitsu.sf.akka.http.socketio
+
+import org.specs2.mutable._
+
+class HelloWorldSpec extends Specification {
+
+  "HelloWorld" should {
+    "#run" in {
+      "print message" in {
+        val helloworld = new HelloWorld with MockOutPut {
+        }
+        helloworld.run()
+        helloworld.message must_== "Hello, World."
+        done
+      }
+    }
+  }
+
+  trait MockOutPut extends Output {
+    var message = ""
+
+    override def print(message: String): Unit = {
+      this.message = message
+    }
+  }
+
+}
---
+++ src/test/scala/arimitsu/sf/akka/http/socketio/HelloWorldTest.scala
@@ -0,0 +1,22 @@
+package arimitsu.sf.akka.http.socketio
+
+import org.scalatest._
+
+class HelloWorldTest extends FlatSpec with Matchers {
+
+  "HelloWorld#run" should "set message" in {
+    val helloworld = new HelloWorld with MockOutPut {
+    }
+    helloworld.run()
+    helloworld.message should be("Hello, World.")
+  }
+
+  trait MockOutPut extends Output {
+    var message = ""
+
+    override def print(message: String): Unit = {
+      this.message = message
+    }
+  }
+
+}
